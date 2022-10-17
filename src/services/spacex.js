e182e76692440cf58fcb7023024e2c31ae7b4104
Daniel Oladimeji <hi.danieladedokun@gmail.com>
2022-10-17 10:23:36 +0100

    Added: SpaceX starlink statelite api

--- src/services/spacex.js
+++ src/services/spacex.js
@@ -40,6 +40,7 @@ const getRockets = async () => {
   return rockets;
 };
 
+//get dragons data from SpaceX API
 const getDragons = async () => {
   const res = await axios.get("https://api.spacexdata.com/v4/dragons");
   const dragons = []
@@ -77,7 +78,71 @@ const getDragons = async () => {
   return dragons;
 }
 
+//get dragons data from SpaceX API
+const getStarlinkSats = async () => {
+  const res = await axios.get("https://api.spacexdata.com/v4/starlink");
+  const starlinkSats = []
+  res.data.forEach((starlinksat) => {
+    starlinksat.push({
+      starlinksatId: starlinksat.id,
+      starlinkSatlaunchId: starlinkSat.launch,
+      properties: {
+        version: starlinkSat.version,
+        height_km: starlinkSat.height_km,
+        latitude: starlinkSat.latitude,
+        longitude: starlinkSat.longitude,
+        velocity_kms: starlinkSat.velocity_kms
+      },
+      spaceTrack: {
+        CCSDS_OMM_VERS: starlinkSat.CCSDS_OMM_VERS,
+        COMMENT: starlinkSat.COMMENT,
+        CREATION_DATE: starlinkSat.CREATION_DATE,
+        ORIGINATOR: starlinkSat.ORIGINATOR,
+        OBJECT_NAME: starlinkSat.OBJECT_NAME,
+        OBJECT_ID: starlinkSat.OBJECT_ID,
+        CENTER_NAME: starlinkSat.CENTER_NAME,
+        REF_FRAME: starlinkSat.REF_FRAME,
+        TIME_SYSTEM: starlinkSat.TIME_SYSTEM,
+        MEAN_ELEMENT_THEORY: starlinkSat.MEAN_ELEMENT_THEORY,
+        EPOCH: starlinkSat.EPOCH,
+        MEAN_MOTION: starlinkSat.MEAN_MOTION,
+        ECCENTRICITY: starlinkSat.ECCENTRICITY,
+        INCLINATION: starlinkSat.INCLINATION,
+        RA_OF_ASC_NODE: starlinkSat.RA_OF_ASC_NODE,
+        ARG_OF_PERICENTER: starlinkSat.ARG_OF_PERICENTER,
+        MEAN_ANOMALY: starlinkSat.MEAN_ANOMALY,
+        EPHEMERIS_TYPE: starlinkSat.EPHEMERIS_TYPE,
+        CLASSIFICATION_TYPE: starlinkSat.CLASSIFICATION_TYPE,
+        NORAD_CAT_ID: starlinkSat.NORAD_CAT_ID,
+        ELEMENT_SET_NO: starlinkSat.ELEMENT_SET_NO,
+        REV_AT_EPOCH: starlinkSat.REV_AT_EPOCH,
+        BSTAR: starlinkSat.BSTAR,
+        MEAN_MOTION_DOT: starlinkSat.MEAN_MOTION_DOT,
+        MEAN_MOTION_DDOT: starlinkSat.MEAN_MOTION_DDOT,
+        SEMIMAJOR_AXIS: starlinkSat.SEMIMAJOR_AXIS,
+        PERIOD: starlinkSat.PERIOD,
+        APOAPSIS: starlinkSat.APOAPSIS,
+        PERIAPSIS: starlinkSat.PERIAPSIS,
+        OBJECT_TYPE: starlinkSat.OBJECT_TYPE,
+        RCS_SIZE: starlinkSat.RCS_SIZE,
+        COUNTRY_CODE: starlinkSat.COUNTRY_CODE,
+        LAUNCH_DATE: starlinkSat.LAUNCH_DATE,
+        SITE: starlinkSat.SITE,
+        DECAY_DATE: starlinkSat.DECAY_DATE,
+        DECAYED: starlinkSat.DECAYED,
+        FILE: starlinkSat.FILE,
+        GP_ID: starlinkSat.GP_ID,
+        TLE_LINE0: starlinkSat.TLE_LINE0,
+        TLE_LINE1: starlinkSat.TLE_LINE1,
+        TLE_LINE2: starlinkSat.TLE_LINE2
+      }
+    });
+  });
+  return starlinkSats;
+}
+
 module.exports = {
   getRockets,
   getDragons,
+  getStarlinkSats,
 };
